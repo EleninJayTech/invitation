19042f007c9e6d524ec4720aab8edda2455007e4
elenin <eleninjaytech@gmail.com>
2022-08-28 19:15:22 +0900

    카카오 테스트

--- js/index.js
+++ js/index.js
@@ -88,4 +88,25 @@ jQuery(function($){
 		e.preventDefault();
 		return false;
 	});
+
+	// init 체크
+	if (!Kakao.isInitialized()) {
+		Kakao.init('0833edef80e95997fef5dc31428b7439');
+	}
+	$(".kakao_link_btn").on('click', function(e){
+		let shareURL = "https://eleninjaytech.github.io/invitation";
+		if( $(this).data('type') === 'new' ){
+			shareURL = "https://eleninjaytech.github.io/invitation/new.html";
+		}
+
+		Kakao.Share.sendDefault({
+			objectType: 'text',
+			text:
+				'기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.',
+			link: {
+				mobileWebUrl:shareURL,
+				webUrl:shareURL,
+			},
+		});
+	});
 });
